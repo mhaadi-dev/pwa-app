32394cce5c431e2b75e35221c8a463622ab2fc48
mhadi.centrox <mhadi.centrox@gmail.com>
2025-03-09 15:46:12 +0500

    pwa commited 2

--- app/lib/connectDb.ts
+++ app/lib/connectDb.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
   if (mongoose.connection.readyState === 0) {
-    await mongoose.connect("mongodb+srv://mhadicentrox:thisismongopass@cluster0.osoxb.mongodb.net/" as string);
+    await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URI as string);
     console.log("MongoDB Connected");
   }
 };
